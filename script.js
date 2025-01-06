bb9e06c65e537a210210d3477ef30db4e10e7ead
Dr-Lepic <mbmahbub007@gmail.com>
2025-01-06 20:42:21 +0600

    function to draw head

--- script.js
+++ script.js
@@ -20,4 +20,79 @@ let score = 0;
 let gameOver = false;
 
 
-// 
+// Generating a random position for the food
+function randomPosition(max) {
+    return Math.floor(Math.random() * ( max / boxSize )) * boxSize;
+}
+
+// Snake's head
+function drawHead(head){
+    // head shape
+    ctx.fillStyle = "green";
+    ctx.beginPath();
+    ctx.arc (
+        head.x + boxSize / 2,
+        head.y + boxSize / 2,
+        boxSize / 2,
+        0,
+        2 * Math.PI
+    )
+
+    ctx.fill();
+
+    // eyes
+    ctx.fillStyle = "white";
+    ctx.beginPath();
+    ctx.arc (
+        head.x + boxSize / 4,
+        head.y + boxSize / 4,
+        boxSize / 8,
+        0,
+        2 * Math.PI
+    );
+
+    ctx.fill();
+
+    ctx.beginPath();
+    ctx.arc (
+        head.x + (3 * boxSize) / 4,
+        head.y + boxSize / 4,
+        boxSize / 8,
+        0,
+        2 * Math.PI
+    );
+
+    ctx.fill();
+
+    // pupils
+    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.arc (
+        head.x + boxSize / 4,
+        head.y + boxSize / 4,
+        boxSize / 16,
+        0,
+        2 * Math.PI
+    );
+
+    ctx.fill();
+    ctx.beginPath();
+    ctx.arc (
+        head.x + (3 * boxSize) / 4,
+        head.y + boxSize / 4,
+        boxSize / 16,
+        0,
+        2 * Math.PI
+    );
+
+    ctx.fill();
+
+    // tongue
+    ctx.fillStyle = "red";
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.moveTo(head.x + boxSize / 2, head.y +  boxSize / 2);
+    ctx.lineTo(head.x + boxSize / 2, head.y + boxSize);
+    ctx.stroke();
+    
+}
